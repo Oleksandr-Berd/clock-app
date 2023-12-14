08e315c13a5b61fd91e77b6d5c6b02a55853c035
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-14 16:44:48 +0200

    fck

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 // export const API_LOCATION_KEY = "ipb_live_wamUTMWgTVPDlhC8hB72rYOpWeystNJfWlft2RYc";
 
-export const worldTimeUrl = "http://worldtimeapi.org/api/ip";
+export const worldTimeUrl = "https://worldtimeapi.org/api/ip";
 // export const geoLocationUrl = "https://freegeoip.app/";
 // export const geoLocationUrl = `https://api.ipbase.com/v2/info?apikey=ipb_live_wamUTMWgTVPDlhC8hB72rYOpWeystNJfWlft2RYc&ip=1.1.1.1`
 export const quotesUrl = "https://api.quotable.io/quotes";
